4f80c993e0499109fdae322be19b479612062bae
ldnguyen06 <nguyenmax12321@gmail.com>
2025-03-03 17:08:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Ô sóc nhỏ đấy hỏ',
+    introDesc: 'Ồ hình như ta thấy sự tiêu cực trên người con',
+    btnIntro: 'Đruiiii',
+    title: 'Để ta tìm cho con một người giúp con đánh bay tiêu cực nheee 😙',
+    desc: '',
+    btnYes: 'Vângggg ạaaaaa',
+    btnNo: 'Không đâu nhé :<',
+    question:'Hãy đồng hành cùng người ấy nhé sóc con',
+    btnReply: 'Gửi cho người ấy <3',
+    reply: 'Lê Đình Nguyên <333333333333',
+    mess: 'Chào cậu nhé. Tớ đến để đánh bay tiêu cực của cậu nè😘',
+    messDesc: 'Click vào ô bên dưới để cùng tớ đánh bay tiêu cực nhé',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://web.facebook.com/profile.php?id=100075441447756' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
